64fdbdb5256039e749809c2bbe63addec13a5479
Ari Cummings <ari@aricummings.com>
2026-01-19 17:49:31 -0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,51 @@
+import { Fragment } from 'react';
+
+let pointsSlips = [
+    {name: 'John', date: new Date(), points: 761, hours: 1.2},
+    {name: 'Bob', date: new Date(), points: 458, hours: 1.6},
+    {name: 'Joe', date: new Date(), points: 6952, hours: 16},
+    {name: 'Billy', date: new Date(), points: 10550, hours: 151},
+    {name: 'Fred', date: new Date(), points: 215, hours: 0.7}
+];
+
+function showEnterPointsScreen(){
+    
+}
+
+function App() {
+    return (
+        <div style={{ display: 'flex', flexDirection: 'row', gap: '10px', alignItems: 'center'}}>
+            <p>Staff Points</p>
+            <button>View Points</button>
+            <button onClick={showEnterPointsScreen}>Enter Points</button>
+        </div>
+    );
+}
+
+export default App;
+
+/*
+
+<dl>
+            {pointsSlips.map((point) => {
+                const pointsPerHour = (point.points / point.hours).toFixed(2);
+
+                return (
+                    <Fragment key={point.name}>
+                        <dt>
+                            {point.name}
+                        </dt>
+                        <dd>
+                            <ol>
+                                <li>Date: {point.date.toDateString()}</li>
+                                <li>Points: {point.points}</li>
+                                <li>Hours: {point.hours}</li>
+                                <li>Points per Hour: {pointsPerHour}</li>
+                            </ol>
+                        </dd>
+                    </Fragment>
+                );
+            })}
+        </dl>
+
+ */
